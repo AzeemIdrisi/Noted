0a06487a6569865585be48cd340deb48e1e2cd2b
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-03-02 20:19:55 +0530

    Mapped notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1>Note Name</h1>
-      <p>Note content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
